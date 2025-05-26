18397594c1cbf1e57cd365a2a9bed623717e93d7
rohitssyy <rohitssyy@gmail.com>
2025-05-26 23:10:44 +0530

    Prisma Schema

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,49 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/extension'
 
 const app = new Hono()
+const prisma = new PrismaClient()
+ 
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/signup', (c) => {
+
+  return c.json({
+    message: "Signup Route: "
+  })
+
+})
+
+app.post('/api/v1/login', (c) => {
+  return c.json({
+    message: "Login Route: "
+  })
+})
+
+
+app.post('/api/v1/blog', (c) => {
+  return c.json({
+    message: "Posting Blog Route: "
+  })
+})
+
+app.put("/api/v1/blog", (c) => {
+
+})
+
+app.get("/api/v1/blog/:id", (c) => {
+
+})
+
+
+
+app.get("/api/v1/blog/bulk", (c) => {
+
+  return c.text("")
+})
+
+app.get("/", (c) => {
+  return c.text("hello")
 })
 
 export default app
