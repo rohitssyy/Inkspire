9838558a61f8a9c5b1a8c6fcee05e717b666263a
rohitssyy <rohitssyy@gmail.com>
2025-06-09 21:21:50 +0530

    updated signup and Login

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,42 +1,85 @@
 import { Hono } from 'hono';
 import { PrismaClient } from './generated/prisma/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL:string
+    DATABASE_URL: string
+    JWT_SECRET: string
   }
 }>()
 
+app.use("/api/v1/blog/*", async(c, next){
+  
+  const header = await c.req.header("Authorization")
+
+  if (!header) {
+    c.status(403)
+    return c.json({error:"Unauthorized"})
+  }
+
+  const token = header?.split(" ")[1]
+
+  const verification = await verify(token, c.env.JWT_SECRET);
+   
+
+
+})
 
 app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL
   }).$extends(withAccelerate())
   
-
+  console.log("SignUp Route Started: ");
+  
   const body = await c.req.json()
-
-  await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password,
-      name: body.name
+  console.log(body)
+  try {
+    
+   const UserCreating = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name
       },
     })
-   
+    
+    const jwt = await sign({id: UserCreating.id}, c.env.JWT_SECRET, 'HS256')
+    return c.json({jwt})
+  } catch (e) {
+    c.status(403);
+    return c.json({e:"Error while signing up "})
+  }
   
-  
-  return c.json({
-    message: "Signup Route: "
-  })
+
 
 })
 
-app.post('/api/v1/login', (c) => {
-  return c.json({
-    message: "Login Route: "
+app.post('/api/v1/login', async (c) => {
+ 
+  console.log("Login Route Started:")
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+
+  const body = await c.req.json();
+
+  const LoginUser = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
+    }
   })
+
+  if (!LoginUser) {
+     c.status(403);
+    return c.json({Error: "User not found"})
+  }
+
 })
 
 
